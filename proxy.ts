b702ebf578400d359d5649a5e89bf1500ff48131
lorabucko <lorabucko@gmail.com>
2026-08-08 06:24:05 +0200

    fix 9

--- proxy.ts
+++ proxy.ts
@@ -76,7 +76,7 @@ export async function proxy(request: NextRequest) {
   }
 
   if (accessToken && isPublicRoute) {
-    return NextResponse.redirect(new URL('/profile', request.url))
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   return NextResponse.next()
