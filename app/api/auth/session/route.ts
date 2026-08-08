ec63d19a78a304c07d36b80b8d046791773f4e4b
lorabucko <lorabucko@gmail.com>
2026-08-08 05:19:03 +0200

    fixed 7

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 import { api } from '../../api'
+import { parseSetCookie } from 'cookie'
 import { isAxiosError } from 'axios'
 import { logErrorResponse } from '../../_utils/utils'
 
